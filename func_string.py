ab733a3adf82879679de51f98b0bb50324311622
Ricardo Antonio Jeronymo <cin_rjeronymo@uolinc.com>
2019-12-12 15:39:28 -0300

    Funcao String

---
+++ func_string.py
@@ -0,0 +1,11 @@
+#!#/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def say_hello(texto):
+    print(f'hello, {texto}! e o tamanho {len(texto)}')
+
+def main():
+    print (say_hello('Teste'))
+   
+if __name__ == '__main__':
+    main()
